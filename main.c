7643c9fb60b7317fe9ddfa3839ab89d01839c265
eunnbyeol <113367529+eunnbyeol@users.noreply.github.com>
2022-10-18 16:58:56 +0900

    05

--- main.c
+++ main.c
@@ -2,20 +2,23 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void f(void);
+void sub(void);
 
-int i;
 int main(int argc, char *argv[]) {
-	for (i=0; i<5; i++)
-	{
-		f();
-	}
+	
+	int i;
+	for(i=0; i<3; i++)
+	    sub();
+
 	return 0;
 }
 
-void f()
+void sub()
 {
-	for(i=0; i<10; i++)
-	    printf("#");
+	int auto_count = 0; //동적할당 
+	static int static_count = 0; //정적할당 
+	auto_count++; //변수 1증가 
+	static_count++;
+	printf("auto_count=%d\n", auto_count); //변수내용확인  
+	printf("statoc_count=%d\n", static_count);
 }
-
