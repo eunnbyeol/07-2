ed6211d309d47b5742894e25bfa1059291d198ec
eunnbyeol <113367529+eunnbyeol@users.noreply.github.com>
2022-10-18 17:09:41 +0900

    06-1

---
+++ Untitled6.c
@@ -0,0 +1,5 @@
+extern int all_flies;
+void sub()
+{
+	all_flies = -10;
+}
--- main.c
+++ main.c
@@ -2,23 +2,16 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void sub(void);
+int all_flies; 
+
+extern void sub();
 
 int main(int argc, char *argv[]) {
 	
-	int i;
-	for(i=0; i<3; i++)
-	    sub();
+	sub();
+	printf("%d\n", all_flies);
 
 	return 0;
 }
 
-void sub()
-{
-	int auto_count = 0; //동적할당 
-	static int static_count = 0; //정적할당 
-	auto_count++; //변수 1증가 
-	static_count++;
-	printf("auto_count=%d\n", auto_count); //변수내용확인  
-	printf("statoc_count=%d\n", static_count);
-}
+
